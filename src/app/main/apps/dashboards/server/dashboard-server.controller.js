70050e4d69b7be673448ec0550250cce9031f9b0
wkl18749279501 <943948632@qq.com>
2017-05-25 19:39:31 +0800

    ..

--- src/app/main/apps/dashboards/server/dashboard-server.controller.js
+++ src/app/main/apps/dashboards/server/dashboard-server.controller.js
@@ -3,35 +3,78 @@
 
     angular
         .module('app.dashboards.server')
-        .controller('DashboardServerController', BigChart);
+        .controller('DashboardServerController', DashboardServerController);
 
     /** @ngInject */
-    function BigChart($scope, $http) {
+    function DashboardServerController($scope, $interval, DashboardData, $http) {
         var vm = this;
-        vm.charts = {
-            options: {
-                chart: {
-                    type: 'line',
-                    zoomType: 'x'
+
+        // Data
+        vm.dashboardData = DashboardData;
+        // $http({
+        //     method: "get",
+        //     url: "app/data/dashboard/analytics/a.json",
+        //     headers: { "Content-Type": "application/json" }
+        // }).success(function(d) {
+
+        //     vm.aa = d.orders;
+
+        // }).error(function(error) {});
+        // Widget 1
+        vm.widget1 = {
+            title: "资金流水记录",
+            chart: {
+                options: {
+                    chart: {
+                        type: 'lineChart',
+                        color: ['#4caf50', '#3f51b5', '#ff5722'],
+                        height: 320,
+                        margin: {
+                            top: 32,
+                            right: 32,
+                            bottom: 32,
+                            left: 48
+                        },
+                        useInteractiveGuideline: true,
+                        clipVoronoi: false,
+                        interpolate: 'cardinal',
+                        x: function(d) {
+                            return d.x;
+                        },
+                        y: function(d) {
+                            return d.y;
+                        },
+                        xAxis: {
+                            tickFormat: function(d) {
+                                return d + ' min.';
+                            },
+                            showMaxMin: false
+                        },
+                        yAxis: {
+                            tickFormat: function(d) {
+                                return d + ' MB';
+                            }
+                        },
+                        interactiveLayer: {
+                            tooltip: {
+                                gravity: 's',
+                                classes: 'gravity-s'
+                            }
+                        },
+                        legend: {
+                            margin: {
+                                top: 8,
+                                right: 0,
+                                bottom: 32,
+                                left: 0
+                            },
+                            rightAlign: false
+                        }
+                    }
                 },
-                tooltip: {
-                    xDateFormat: '%Y-%m-%d %H:%M:%S',
-                    valueDecimals: 2
-                },
-                xAxis: {
-                    type: 'datetime',
-                    dateTimeLabelFormats: {
-                        hour: '%H:%M'
-                    },
-                    minRange: 1000, // 不能放大超过1s
-                    minTickInterval: 1000 // 放大间隔最小为1s
-                }
-            },
-            series: {　　　　　　　　 data: [100.0, 99.0, 100.0, 98.039216, 100.0, 99.0, 100.0, 100.0, 100.0, 100.0, 97.087379, 99.0, 99.009901, 100.0, 99.0, 100.0, 99.009901, 100.0, 100.0, 98.039216, 100.0, 100.0, 100.0, 99.009901, 99.009901, 100.0, 99.009901, 100.0, 99.0, 100.0, 100.0, 99.0, 100.0, 99.009901, 100.0, 99.0, 99.0, 99.009901, 99.009901, 100.0, 100.0, 99.009901, 100.0, 99.009901, 100.0, 99.0, 98.039216, 100.0, 99.0, 100.0, 99.0, 100.0, 100.0, 100.0, 100.0, 100.0, 99.0, 100.0, 100.0], 　　　　　　　　name: '192.168.17.136'　　　　　　 },
-            　　　　　title: {
-                text: null
+                data: vm.dashboardData.widget1.chart
             }
-        }
+        };
         $http({
             method: "get",
             url: "app/data/dashboard/analytics/a.json",
@@ -41,72 +84,6 @@
             vm.aa = d.orders;
 
         }).error(function(error) {});
-
-
-        // Data
-        // vm.dashboardData = DashboardData;
-
-        // Widget 1
-        // vm.widget1 = {
-        //     title: vm.dashboardData.widget1.title,
-        //     chart: {
-        //         options: {
-        //             chart: {
-        //                 type: 'lineChart',
-        //                 // color: ['#4caf50', '#3f51b5', '#ff5722'],
-        //                 color: ['#4caf50'],
-        //                 height: 320,
-        //                 margin: {
-        //                     top: 32,
-        //                     right: 32,
-        //                     bottom: 32,
-        //                     left: 48
-        //                 },
-        //                 useInteractiveGuideline: true,
-        //                 clipVoronoi: false,
-        //                 interpolate: 'cardinal',
-        //                 x: function(d) {
-
-        //                     return d.x;
-        //                 },
-        //                 y: function(d) {
-        //                     return d.y;
-        //                 },
-        //                 xAxis: {
-        //                     tickFormat: function(d) {
-        //                         console.log(d)
-        //                         return d + ' min.';
-        //                     },
-        //                     showMaxMin: false
-        //                 },
-        //                 yAxis: {
-        //                     tickFormat: function(d) {
-        //                         return d + ' MB';
-        //                     }
-        //                 },
-        //                 interactiveLayer: {
-        //                     tooltip: {
-        //                         gravity: 's',
-        //                         classes: 'gravity-s'
-        //                     }
-        //                 },
-        //                 legend: {
-        //                     margin: {
-        //                         top: 8,
-        //                         right: 0,
-        //                         bottom: 32,
-        //                         left: 0
-        //                     },
-        //                     rightAlign: false
-        //                 }
-        //             }
-        //         },
-        //         data: vm.dashboardData.widget1.chart
-        //     }
-        // };
-
-
-
 
     }
 })();
--- src/app/main/apps/dashboards/server/dashboard-server.module.js
+++ src/app/main/apps/dashboards/server/dashboard-server.module.js
@@ -4,8 +4,8 @@
     angular
         .module('app.dashboards.server', [
             // 3rd Party Dependencies
-            'nvd3'
-
+            'nvd3',
+            'datatables'
         ])
         .config(config);
 
@@ -19,13 +19,17 @@
                     templateUrl: 'app/main/apps/dashboards/server/dashboard-server.html',
                     controller: 'DashboardServerController as vm'
                 }
-            }
-
-
+            },
+            resolve: {
+                DashboardData: function(msApi) {
+                    return msApi.resolve('dashboard.server@get');
+                }
+            },
+            bodyClass: 'dashboard-server'
         });
 
         // Api
-
-
+        msApiProvider.register('dashboard.server', ['app/data/dashboard/server/data.json']);
     }
+
 })();
